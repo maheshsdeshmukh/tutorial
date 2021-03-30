9ece8fe87050b0bc7a22c49c3e7321129e0e4119
Mahesh Deshmukh <mahesh.s.deshmukh@gmail.com>
2021-03-30 11:45:57 +0000

    demo app

---
+++ src/main/java/com/javabydeveloper/service/MessageService.java
@@ -0,0 +1,11 @@
+package com.javabydeveloper.service;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageService {
+  
+  public String getSubscriptionMessage(String user) {
+    
+    return "Hello "+user+", Thanks for the subscription!";
+  }
+}
